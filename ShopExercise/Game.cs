b97667683f638e6bd54da261a76a1cb077a4b60c
Peyton <peytonsemail2000@gmail.com>
2021-09-20 16:59:21 -0500

    Created the Shop Exercise and created multiple functions

---
+++ ShopExercise/Game.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace ShopExercise
+{
+    public struct Item
+    {
+        public int Cost;
+        public string Name;
+    }
+
+    class Game
+    {
+        private Player _player;
+        private Shop _shop;
+        private bool _gameOver;
+        private int _currentScene;
+
+        public void Run()
+        {
+
+        }
+
+        private void Start()
+        {
+
+        }
+
+        private void Update()
+        {
+
+        }
+
+        private void End()
+        {
+
+        }
+
+        private void InitializeItems()
+        {
+
+        }
+
+        int GetInput(string description, params string[] options)
+        {
+            string input = "";
+            int inputRecieved = -1;
+
+            while (inputRecieved == -1)
+            {
+                //Print options
+                Console.WriteLine(description);
+
+                for (int i = 0; i < options.Length; i++)
+                {
+                    Console.WriteLine((i + 1) + "." + options[i]);
+                }
+                Console.Write("> ");
+
+                //Get input from player
+                input = Console.ReadLine();
+
+                //If the player typed an int...
+                if (int.TryParse(input, out inputRecieved))
+                {
+                    //...decrement the input and check if it's within the bounds of the array
+                    inputRecieved--;
+                    if (inputRecieved < 0 || inputRecieved >= options.Length)
+                    {
+                        //Set input recieved to be the default value
+                        inputRecieved = -1;
+                        //Display error message
+                        Console.WriteLine("Invalid Input");
+                        Console.ReadKey(true);
+                    }
+                }
+                //If the player didn't type an int
+                else
+                {
+                    //Set input recieved to be the default value
+                    inputRecieved = -1;
+                    Console.WriteLine("Invalid Input");
+                    Console.ReadKey(true);
+                }
+
+                Console.Clear();
+            }
+
+            return inputRecieved;
+        }
+
+        public void Save()
+        {
+            //Create a new stream writer
+            StreamWriter writer = new StreamWriter("Inventory.txt");
+
+            //Save current enemy index
+            writer.WriteLine(_inventory);
+
+            //Save player and enemy stats
+            _player.Save(writer);
+            _currentEnemy.Save(writer);
+
+            //Close the writer when done saving
+            writer.Close();
+        }
+
+    }
+}
---
+++ ShopExercise/Player.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace ShopExercise
+{
+    class Player
+    {
+        private int _gold;
+        private Item[] _inventory;
+
+        public int Gold
+        {
+            get
+            {
+                return _gold;
+            }
+        }
+
+        public Item[] Inventory
+        {
+            get
+            {
+                return _inventory;
+            }
+        }
+
+        public Player(int gold)
+        {
+            _gold = gold;
+        }
+
+        public void Buy(Item item)
+        {
+            //Create a new array with the size of the old array
+            Item[] newArray = new Item[_inventory.Length + 1];
+
+            //Copy the values from the old array
+            for (int i = 0; i < _inventory.Length; i++)
+            {
+                newArray[i] = _inventory[i];
+            }
+
+            //set the last index to be the new item
+            ///Commented out code
+            //newArray[newArray.Length - 1] = value;
+
+            //return the new array
+            return;
+        }
+
+        public string[] GetItemNames()
+        {
+            string[] itemNames = new string[_inventory.Length];
+
+            for (int i = 0; i < _inventory.Length; i++)
+            {
+                itemNames[i] = _inventory[i].Name;
+            }
+
+            return itemNames;
+        }
+
+        /*
+        public override void Save(StreamWriter writer)
+        {
+            writer.WriteLine(_gold);
+            base.Save(writer);
+            writer.WriteLine(_inventory);
+        }
+
+        public override bool Load(StreamReader reader)
+        {
+            //if the base loading function fails...
+            if (!base.Load(reader))
+            {
+                //...return false
+                return false;
+            }
+
+            //If the current line can't be converte into an int...
+            if (!int.TryParse(reader.ReadLine(), out _inventory))
+            {
+                //...return false
+                return false;
+            }
+
+            //Return whether or not the item was equipped successfully
+            return true;
+            */
+        
+    }
+}
